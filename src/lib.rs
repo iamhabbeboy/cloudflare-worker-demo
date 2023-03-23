7e553be5923c7b529d26088d13ada373c1905b40
Abiodun Azeez <iamhabbeboy@gmail.com>
2023-03-23 12:49:49 +0100

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use serde_json::json;
+use worker::*;
+
+mod utils;
+
+fn log_request(req: &Request) {
+    console_log!(
+        "{} - [{}], located at: {:?}, within: {}",
+        Date::now().to_string(),
+        req.path(),
+        req.cf().coordinates().unwrap_or_default(),
+        req.cf().region().unwrap_or_else(|| "unknown region".into())
+    );
+}
+
+#[event(fetch)]
+pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
+    log_request(&req);
+
+    // Optionally, get more helpful error messages written to the console in the case of a panic.
+    utils::set_panic_hook();
+
+    // Optionally, use the Router to handle matching endpoints, use ":name" placeholders, or "*name"
+    // catch-alls to match on specific patterns. Alternatively, use `Router::with_data(D)` to
+    // provide arbitrary data that will be accessible in each route via the `ctx.data()` method.
+    let router = Router::new();
+
+    // Add as many routes as your Worker needs! Each route will get a `Request` for handling HTTP
+    // functionality and a `RouteContext` which you can use to  and get route parameters and
+    // Environment bindings like KV Stores, Durable Objects, Secrets, and Variables.
+    router
+        .get("/", |_, _| Response::ok("Hello from Logrocket Workers!"))
+        .get_async("/json/:id", |_, ctx| async move {
+            if let Some(id) = ctx.param("id") {
+                match id.parse::<u32>() {
+                    Ok(id) => {
+                        let json = json!({
+                            "id": id,
+                            "message": "Hello from Logrocket Workers!",
+                            "timestamp": Date::now().to_string(),
+                        });
+                        return Response::from_json(&json);
+                    }
+                    Err(_) => return Response::error("Bad Request", 400),
+                }
+        };
+        Response::error("Bad Request", 400)
+        })
+        .get("/worker-version", |_, ctx| {
+            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
+            Response::ok(version)
+        })
+        .run(req, env)
+        .await
+}
